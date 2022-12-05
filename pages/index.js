a86d2ddd3fd4584b1db3eb717091ffbf5c7d9613
张宏年 <zhanghongnian@hiorka.com>
2022-12-05 17:24:48 +0800

    fix: update twice

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Yasuo landed!</a>
+         Your are right
         </h1>
 
         <p className={styles.description}>
